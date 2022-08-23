4cf1b8717bbb244637477796163db7103b315a3e
onetester <onetester@testcompany.com>
2022-08-23 21:27:21 +0100

    changes in main program

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def doCalculate(a, b):
         return a + b
 
 def printTestMessage():
-	print("This is a test message!!!")
+	print("This is a test message !!!")
 
 if __name__ == '__main__':
         printWelcomeMessage()
